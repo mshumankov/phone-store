6757f6b4474e6a66cd9e3a91708ea31faa45334f
Martin Shumankov <martin.shumankov@gmail.com>
2020-06-05 01:13:06 +0300

    Updates

---
+++ precache-manifest.6deb6552014c4ba222fc85a17b4f25c4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21db13e0f3908734f569a808d8975b4",
+    "url": "/phone-store/index.html"
+  },
+  {
+    "revision": "e04be5999aa06098fe70",
+    "url": "/phone-store/static/css/main.cef6119f.chunk.css"
+  },
+  {
+    "revision": "06dcffb2eb616d8f3117",
+    "url": "/phone-store/static/js/2.6349427d.chunk.js"
+  },
+  {
+    "revision": "8736c11eb14375cf022a7311cd089c7a",
+    "url": "/phone-store/static/js/2.6349427d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04be5999aa06098fe70",
+    "url": "/phone-store/static/js/main.d8717abb.chunk.js"
+  },
+  {
+    "revision": "fe511d2306ebdf4e04fe",
+    "url": "/phone-store/static/js/runtime-main.3432d1b2.js"
+  },
+  {
+    "revision": "3841baf92f63b18cbc9e04ce6cd78c92",
+    "url": "/phone-store/static/media/girlWithPhone.3841baf9.jpg"
+  },
+  {
+    "revision": "6f6ea2d89cae24e6291c23cba898e7bc",
+    "url": "/phone-store/static/media/girlWithPhone2.6f6ea2d8.jpg"
+  },
+  {
+    "revision": "fdb0efaacaa46c1a524e87ddfc94c645",
+    "url": "/phone-store/static/media/phone-hand4.fdb0efaa.jpg"
+  },
+  {
+    "revision": "b0f4528c9669561631ea5ec459be2f2e",
+    "url": "/phone-store/static/media/phone-purple.b0f4528c.jpg"
+  },
+  {
+    "revision": "c64bab6f63cbde29f6804714ba5d9fc7",
+    "url": "/phone-store/static/media/phoneHand1.c64bab6f.jpg"
+  },
+  {
+    "revision": "c1891f751dfec3cbc2c0b0d1077eaebe",
+    "url": "/phone-store/static/media/phoneHand2.c1891f75.jpg"
+  },
+  {
+    "revision": "5866a4a94916b3af6531eb8b41cdbc69",
+    "url": "/phone-store/static/media/phoneHand3.5866a4a9.jpg"
+  },
+  {
+    "revision": "0f796641aac4fc4ef78cb3fb6a39d8ed",
+    "url": "/phone-store/static/media/phoneNight.0f796641.jpg"
+  }
+]);
